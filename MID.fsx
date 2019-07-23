cb0d71fdc91d079699be023e783089918e440521
Stefano Torneo <50771226+s-torneo@users.noreply.github.com>
2019-07-23 19:13:46 +0200

    New Commit

--- MID.fsx
+++ MID.fsx
@@ -2,14 +2,11 @@
 //#load "Controls.fsx"
 open System.Windows.Forms
 open System.Drawing
-open System.ComponentModel
+open System.IO
 
 //open Matrix
 //open Controls
 
-let PI = System.Math.PI
-let deg2rad (a:single) =
-  PI * float(a / 180.f) |> single
 // Libreria
 
 type WVMatrix () =
@@ -57,23 +54,12 @@ type LWCControl() =
   let wv = WVMatrix()
 
   let mutable sz = SizeF(120.f, 120.f)
-  
   let mutable pos = PointF()
-
   let mutable parent : LWCContainer option = None
-
-  let mutable Color = Brushes.Red
-
+  let mutable Color = null
   let mutable Selected = false
-
-  let mutable Image = null
-
-  let mutable manigliaS = PointF()
-
-  let mutable manigliaA = PointF()
-
+  let mutable img : Image = null
   let mutable cclass = null
-
   let mutable name = null
 
   member this.WV = wv
@@ -118,18 +104,6 @@ type LWCControl() =
       wv.TranslateV(-pos.X, -pos.Y)
       this.Invalidate()
    
-  member this.ManigliaS
-    with get() = manigliaS
-    and set(v) =
-      manigliaS <- v
-      this.Invalidate()
-  
-  member this.ManigliaA
-    with get() = manigliaA
-    and set(v) =
-      manigliaA <- v
-      this.Invalidate()
-
   member this.Class
     with get() = cclass 
     and set(v) = 
@@ -159,9 +133,9 @@ type LWCControl() =
       this.Invalidate()
   
   member this.image
-    with get() = Image
-    and set(v:Bitmap) = 
-      Image <- v
+    with get() = img
+    and set(v:Image) = 
+      img <- v
 
   member this.Left = pos.X
   
@@ -175,21 +149,14 @@ and LWCContainer() as this =
 
   let controls = System.Collections.ObjectModel.ObservableCollection<LWCControl>()
 
-  let mutable newbox = None
+  let mutable newplanet = None
   let mutable drag = None
-  let mutable resize = None
-  let mutable selectionButton = false
-  let mutable selectionMode = false
-  let mutable removeMode = false
-  let mutable imageMode = false
-  let mutable resizeMode = false
-  let mutable myPicture : Bitmap = new Bitmap(@"C:\Users\step9\OneDrive\Desktop\Documenti\PDF\UNIPI\Dispense\Terzo anno\Primo Semestre\PI\PreparazioneMID\earth.jpg")
-  let timer = new Timer(Interval=100)
-  let mkrect (sx, sy) (ex, ey) =
-    Rectangle(min sx ex, min sy ey, abs(sx - ex), abs(sy - ey))
-
-  let bimage = new Button(Text="Image", Location=Point(10, 220))
-  let mutable new_planet = false
+  let mutable newMode = false
+  let mutable myPicture : Bitmap = null
+  let timer = new Timer(Interval=25)
+  let timer2 = new Timer(Interval=35)
+  let mutable count1 = 0
+  let mutable count2 = 0
 
   do 
     this.SetStyle(ControlStyles.AllPaintingInWmPaint ||| ControlStyles.OptimizedDoubleBuffer, true)
@@ -197,92 +164,125 @@ and LWCContainer() as this =
       for i in e.NewItems do
         (i :?> LWCControl).Parent <- Some(this)
       )
-    let bmpPicture : Bitmap = new Bitmap(@"C:\Users\step9\OneDrive\Desktop\Documenti\PDF\UNIPI\Dispense\Terzo anno\Primo Semestre\PI\PreparazioneMID\ship.png")
-    controls.Add(LWButton(Position=PointF(100.f, 100.f),ClientSize=SizeF(30.f, 30.f),color=(Brushes.Red),selected=false,Class="ship",image=bmpPicture))
-    controls.Add(LWButton(Name="New Planet",Position=PointF(10.f, 10.f),ClientSize=SizeF(55.f,20.f),color=Brushes.Yellow))
-    controls.Add(LWButton(Name="Up",Position=PointF(10.f, 40.f),ClientSize=SizeF(55.f,20.f),color=Brushes.Yellow))
-    controls.Add(LWButton(Name="Down",Position=PointF(10.f, 70.f),ClientSize=SizeF(55.f,20.f),color=Brushes.Yellow))
-    controls.Add(LWButton(Name="Left",Position=PointF(10.f, 100.f),ClientSize=SizeF(55.f,20.f),color=Brushes.Yellow))
-    controls.Add(LWButton(Name="Right",Position=PointF(10.f, 130.f),ClientSize=SizeF(55.f,20.f),color=Brushes.Yellow))
-    controls.Add(LWButton(Name="RotateR",Position=PointF(10.f, 160.f),ClientSize=SizeF(55.f,20.f),color=Brushes.Yellow))
-    controls.Add(LWButton(Name="RotateL",Position=PointF(10.f, 190.f),ClientSize=SizeF(55.f,20.f),color=Brushes.Yellow))
-    controls.Add(LWButton(Name="ZoomIn",Position=PointF(10.f, 220.f),ClientSize=SizeF(55.f,20.f),color=Brushes.Yellow))
-    controls.Add(LWButton(Name="ZoomOut",Position=PointF(10.f, 250.f),ClientSize=SizeF(55.f,20.f),color=Brushes.Yellow))
-    controls.Add(LWButton(Name="Dimension+",Position=PointF(10.f, 280.f),ClientSize=SizeF(55.f,20.f),color=Brushes.Yellow))
-    controls.Add(LWButton(Name="Dimension-",Position=PointF(10.f, 310.f),ClientSize=SizeF(55.f,20.f),color=Brushes.Yellow))
+    let dlg = new OpenFileDialog()
+    dlg.Filter <- "|*.BMP;*.JPG;*.GIF;*.PNG"
+    if dlg.ShowDialog() = DialogResult.OK then
+      let imagename = dlg.FileName
+      myPicture <- new Bitmap(imagename)
+    controls.Add(LWButton(Position=PointF(100.f, 100.f),ClientSize=SizeF(30.f, 30.f),selected=false,Class="ship", image=myPicture))
+    controls.Add(LWButton(Class="Op", Name="New Planet",Position=PointF(10.f, 10.f),ClientSize=SizeF(65.f,20.f),color=Brushes.SkyBlue))
+    controls.Add(LWButton(Class="Op", Name="Up",Position=PointF(10.f, 40.f),ClientSize=SizeF(65.f,20.f),color=Brushes.SkyBlue))
+    controls.Add(LWButton(Class="Op", Name="Down",Position=PointF(10.f, 70.f),ClientSize=SizeF(65.f,20.f),color=Brushes.SkyBlue))
+    controls.Add(LWButton(Class="Op", Name="Left",Position=PointF(10.f, 100.f),ClientSize=SizeF(65.f,20.f),color=Brushes.SkyBlue))
+    controls.Add(LWButton(Class="Op", Name="Right",Position=PointF(10.f, 130.f),ClientSize=SizeF(65.f,20.f),color=Brushes.SkyBlue))
+    controls.Add(LWButton(Class="Op", Name="RotateR",Position=PointF(10.f, 160.f),ClientSize=SizeF(65.f,20.f),color=Brushes.SkyBlue))
+    controls.Add(LWButton(Class="Op", Name="RotateL",Position=PointF(10.f, 190.f),ClientSize=SizeF(65.f,20.f),color=Brushes.SkyBlue))
+    controls.Add(LWButton(Class="Op", Name="ZoomIn",Position=PointF(10.f, 220.f),ClientSize=SizeF(65.f,20.f),color=Brushes.SkyBlue))
+    controls.Add(LWButton(Class="Op", Name="ZoomOut",Position=PointF(10.f, 250.f),ClientSize=SizeF(65.f,20.f),color=Brushes.SkyBlue))
+    controls.Add(LWButton(Class="Op", Name="Dimension+",Position=PointF(10.f, 280.f),ClientSize=SizeF(65.f,20.f),color=Brushes.SkyBlue))
+    controls.Add(LWButton(Class="Op", Name="Dimension-",Position=PointF(10.f, 310.f),ClientSize=SizeF(65.f,20.f),color=Brushes.SkyBlue))
+    
+    timer.Tick.Add( fun _ ->
+       controls 
+           |> Seq.iter(fun c ->
+           if(c.Class = "ship") then
+             c.WV.TranslateW(5.f, 0.f)
+           )
+       this.Invalidate()
+       count1 <- count1 + 1
+       if (count1 = 15) then
+         count1 <- 0
+         timer.Stop()
+         timer2.Start()
+    )
+    timer2.Tick.Add(fun _ ->
+     controls 
+        |> Seq.iter(fun c ->
+        if(c.Class = "ship") then
+          c.WV.TranslateW(2.f, 0.f)
+         )
+     this.Invalidate()
+     count2 <- count2 + 1
+     if (count2 = 50) then
+       count2 <- 0
+       timer2.Stop()
+    )
    
   member this.LWControls with get() = controls
 
   member this.NewPlanet() =
-    imageMode <- true
-    //resizeMode <- true
+    newMode <- true
+
   member this.Up() =
     controls 
         |> Seq.iter(fun c ->
-        if(c.Class <> "ship" && c.Name = null) then
+        if(c.Class <> "ship" && c.Class <> "Op") then
           c.WV.TranslateV(0.f, 10.f)
-        this.Invalidate()
         )
+    this.Invalidate()
 
   member this.Down() = 
     controls 
         |> Seq.iter(fun c ->
-        if(c.Class <> "ship" && c.Name = null) then
+        if(c.Class <> "ship" && c.Class <> "Op") then
           c.WV.TranslateV(0.f, -10.f)
-        this.Invalidate()
         )
+    this.Invalidate()
   
   member this.Left() = 
     controls 
         |> Seq.iter(fun c ->
-        if(c.Class <> "ship" && c.Name = null) then
+        if(c.Class <> "ship" && c.Class <> "Op") then
           c.WV.TranslateV(10.f, 0.f)
-        this.Invalidate()
         )
+    this.Invalidate()
+
   member this.Right() = 
     controls 
         |> Seq.iter(fun c ->
-        if(c.Class <> "ship" && c.Name = null) then
+        if(c.Class <> "ship" && c.Class <> "Op") then
           c.WV.TranslateV(-10.f, 0.f)
-        this.Invalidate()
         )
+    this.Invalidate()
+
   member this.RotateR() = 
     let cx, cy = this.ClientSize.Width / 2 |> single, this.ClientSize.Height / 2 |> single
     controls 
         |> Seq.iter(fun c ->
-        if(c.Name = null) then
+        if(c.Class <> "Op") then
           c.WV.TranslateV(cx, cy)
           c.WV.RotateV(10.f)
           c.WV.TranslateV(-cx, -cy)
-        this.Invalidate()
         )
+    this.Invalidate()
+
   member this.RotateL() = 
     let cx, cy = this.ClientSize.Width / 2 |> single, this.ClientSize.Height / 2 |> single
     controls 
         |> Seq.iter(fun c ->
-        if(c.Name = null) then
+        if(c.Class <> "Op") then
           c.WV.TranslateV(cx, cy)
           c.WV.RotateV(-10.f)
           c.WV.TranslateV(-cx, -cy)
-        this.Invalidate()
         )
+    this.Invalidate()
+
   member this.ZoomOut() = 
       let cx, cy = this.ClientSize.Width / 2 |> single, this.ClientSize.Height / 2 |> single
       controls 
         |> Seq.iter(fun c ->
-        if(c.Name = null) then
-          //let cx, cy = c.Position.X + c.Width/2.f |> single, c.Position.Y + c.Height/2.f |> single
+        if(c.Class <> "Op") then
           c.WV.TranslateV(cx, cy)
           c.WV.ScaleV(1.1f, 1.1f)
           c.WV.TranslateV(-cx, -cy)
         )
       this.Invalidate()
+
   member this.ZoomIn() = 
       let cx, cy = this.ClientSize.Width / 2 |> single, this.ClientSize.Height / 2 |> single
       controls 
         |> Seq.iter(fun c ->
-        if(c.Name = null) then
-          //let cx, cy = c.Position.X + c.Width/2.f |> single, c.Position.Y + c.Height/2.f |> single
+        if(c.Class <> "Op") then
           c.WV.TranslateV(cx, cy)
           c.WV.ScaleV(1.f/1.1f, 1.f/1.1f)
           c.WV.TranslateV(-cx, -cy)
@@ -292,52 +292,43 @@ and LWCContainer() as this =
   member this.IncrDim() = 
       controls 
         |> Seq.iter(fun c ->
-        if(c.Name = null && c.selected) then
+        if(c.selected) then
           c.ClientSize <- SizeF(c.Width + 10.f, c.Height + 10.f)
         )
       this.Invalidate()
+
   member this.DecrDim() = 
       controls 
         |> Seq.iter(fun c ->
-        if(c.Name = null && c.selected) then
+        if(c.selected) then
           c.ClientSize <- SizeF(c.Width - 10.f, c.Height - 10.f)
         )
       this.Invalidate()
+
   override this.OnMouseDown (e) =
     let oc =
       controls |> Seq.tryFindBack(fun c -> c.HitTest(e.Location))
     match oc with
     | Some c ->
-      controls.Move(controls.IndexOf(c), controls.Count-1) // in modo che prende sempre quello di sopra
       if (c.selected) then
         c.selected <- false
-      else
+      else if (c.Class <> "Op") then
         c.selected <- true
-      //let pt = c.WV.TransformPointV(PointF(single e.X, single e.Y))
-      //let boundingbox = RectangleF(0.f, c.Height/2.f, 10.f, 10.f)
-      //if(boundingbox.Contains(pt)) then
-      if (resizeMode) then
-        resize <- Some(c, e.X, e.Y, "s")
-      //let boundingbox = RectangleF(c.Width/2.f, 0.f, 10.f, 10.f)
-      //if(boundingbox.Contains(pt)) then
-        //resize <- Some(c, e.X, e.Y, "a")
       let dx, dy = e.X - int(c.Position.X), e.Y - int(c.Position.Y)
-      if (selectionMode = false && c.Name = null) then
+      if (c.Class <> "Op") then
         drag <- Some (c, dx, dy)
-      else if (c.Name = null) then
-        drag <- Some (c, e.X, e.Y)
       let p = c.WV.TransformPointV(PointF(single e.X, single e.Y))
       let evt = new MouseEventArgs(e.Button, e.Clicks, int p.X, int p.Y, e.Delta)
       c.OnMouseDown(evt)
       this.Invalidate()
     | None -> 
-        if (imageMode) then
+        if (newMode) then
           let dlg = new OpenFileDialog()
           dlg.Filter <- "|*.BMP;*.JPG;*.GIF;*.PNG"
           if dlg.ShowDialog() = DialogResult.OK then
             let imagename = dlg.FileName
             myPicture <- new Bitmap(imagename)
-          newbox <- Some (e.X, e.Y)
+          newplanet <- Some (e.X, e.Y)
 
   override this.OnMouseUp (e) =
     let oc =
@@ -351,40 +342,18 @@ and LWCContainer() as this =
     | None -> 
       controls 
           |> Seq.iter(fun c ->
-            if(c.Name = null) then
-              c.color <- Brushes.Red  
+            if(c.Class <> "Op") then 
               c.selected <- false
           )
-      resizeMode <- false
-      selectionMode <- false
-    match newbox with
+      timer.Stop()
+    match newplanet with
      | Some(ex, ey) ->
-       (*if (selectionButton = true) then
-          controls 
-          |> Seq.iter(fun c ->
-            let w = single(abs(sx -  e.X))
-            let h = single(abs(sy - e.Y))
-            //let r = mkrect (sx, sy) (int(e.X), int(e.Y))
-            //let pt = c.WV.TransformPointV(PointF(single c.Position.X, single c.Position.Y))
-            let boundingbox = RectangleF(single sx, single sy, w, h)
-            let rect = RectangleF(c.Position.X, c.Position.Y, c.Width, c.Height)
-            //printfn "px: %A y: %A bx: %A by: %A w: %A h: %A" pt.X pt.Y boundingbox.X boundingbox.Y w h
-            if(boundingbox.Contains(rect)) then
-            //printfn "px: %A py: %A sx: %A sy: %A w: %A h: %A" c.Position.X c.Position.Y sx sy w h
-            //if ((c.Position.X > single(sx) && c.Position.X < (single(sx) + w) && c.Position.Y > single(sy) && c.Position.Y < (single(sy) + h)) then
-              c.color <- Brushes.Yellow
-              c.selected <- true
-              selectionMode <- true
-          ) 
-       else *)
-       controls.Add(LWButton(image=myPicture,(*ManigliaA=PointF(single(abs( sx - e.X))/2.f, 0.f),ManigliaS=PointF(0.f, single(abs( sy - e.Y))/2.f),*)Position=PointF(single ex, single ey),ClientSize=SizeF(100.f, 100.f),selected=false))
-       newbox <- None
-       imageMode <- false
-       selectionButton <- false
+       controls.Add(LWButton(image=myPicture,Position=PointF(single ex, single ey),ClientSize=SizeF(100.f, 100.f),selected=false))
+       newplanet <- None
+       newMode <- false
        this.Invalidate()
      | None -> 
        drag <- None
-       resize <- None
 
   override this.OnMouseMove (e) =
     let oc =
@@ -398,24 +367,8 @@ and LWCContainer() as this =
     | None -> () 
     match drag with
     | Some(c, dx, dy) ->
-      if (selectionMode = false ) then
-         c.Position <- PointF(single(e.X - int(dx)),single( e.Y - int(dy)))
-      else
-        controls 
-          |> Seq.iter(fun c ->
-          if(c.selected) then
-            c.Position <- PointF(c.Position.X + single e.X - single dx, c.Position.Y + single e.Y - single dy)
-        )
-        drag <- Some(c,e.X, e.Y)
-      this.Invalidate()
-    | _ -> ()
-    match resize with
-    | Some(c, dx, dy, s) ->
-      if (s = "s") then
-        c.ClientSize <- SizeF(c.Width - (single e.X - single dx), c.Height)
-      else
-        c.ClientSize <- SizeF(c.Width, c.Height - (single e.Y - single dy))
-      resize <- Some (c, e.X, e.Y, s)
+      c.Position <- PointF(single(e.X - int(dx)),single( e.Y - int(dy)))
+      c.selected <- false
       this.Invalidate()
     | _ -> ()
 
@@ -423,42 +376,21 @@ and LWCContainer() as this =
     controls 
     |> Seq.iter(fun c ->
       let bkg = e.Graphics.Save()
-      // esercizio: impostare il rettangolo in client space
       let evt = new PaintEventArgs(e.Graphics, Rectangle(c.PositionInt, c.ClientSizeInt))
-      //bug: non supporta la rotazione
-      //e.Graphics.SetClip(new RectangleF(c.Position, c.ClientSize))
       e.Graphics.Transform <- c.WV.WV // cambiamo la matrice di trasformazione
       c.OnPaint(evt)
       e.Graphics.Restore(bkg)
     )
 
+  override this.OnKeyUp e = 
+    match e.KeyCode with
+    | Keys.D ->
+      timer.Start()
+    | _ -> ()
+
   override this.OnKeyDown e =
     let mutable ship = LWCControl()
     match e.KeyCode with
-    | Keys.W -> 
-      controls 
-      |> Seq.iter(fun c ->
-        if(c.Class = "ship") then
-          c.WV.TranslateW(0.f, -10.f)
-          ship <- c
-      )
-      controls.Move(controls.IndexOf(ship), controls.Count-1) 
-    | Keys.A -> 
-      controls 
-        |> Seq.iter(fun c ->
-        if(c.Class = "ship") then
-          c.WV.TranslateW(-10.f, 0.f)
-          ship <- c
-      )
-      controls.Move(controls.IndexOf(ship), controls.Count-1) 
-    | Keys.S -> 
-      controls 
-        |> Seq.iter(fun c ->
-        if(c.Class = "ship") then
-          c.WV.TranslateW(0.f, 10.f)
-          ship <- c
-        )
-      controls.Move(controls.IndexOf(ship), controls.Count-1) 
     | Keys.D ->
       controls 
         |> Seq.iter(fun c ->
@@ -509,15 +441,10 @@ and LWButton() as this =
   override this.OnPaint(e) =
     let g = e.Graphics
     if (this.image <> null) then
-      //g.FillRectangle(Brushes.Transparent, 0.f, 0.f, this.Width, this.Height)
-      //g.FillRectangle(Brushes.Black, 0.f, 0.f, 10.f, 10.f)
-      //g.FillRectangle(Brushes.Black, this.Width/2.f, 0.f, 10.f, 10.f)
       g.DrawImage(this.image, RectangleF(0.f, 0.f, this.Height,this.Width))
-    else if (this.Width <> 0.f) then
+    else 
       g.FillRectangle(this.color, 0.f, 0.f, this.Width, this.Height)
-      //g.FillRectangle(Brushes.Black, 0.f, this.Height/2.f, 10.f, 10.f)
-      //g.FillRectangle(Brushes.Black, this.Width/2.f, 0.f, 10.f, 10.f)
-      g.DrawString(this.Name, new Font("Calibri", 8.f), Brushes.Black, 0.f, 0.f)
+      g.DrawString(this.Name, new Font("Calibri", 10.f), Brushes.Black, 0.f, 0.f)
   
   override this.OnMouseDown(e) =
     let p = this.Parent
@@ -541,7 +468,7 @@ and LWButton() as this =
 let lwcc = new LWCContainer(Dock=DockStyle.Fill)
 
 let f = new Form(Text="Prova", Location=Point(500,500), Width=1000, Height=500)
-f.Controls.Add(lwcc)
 
+f.Controls.Add(lwcc)
 
 f.Show()
